4580524495ad86ec4d7258c16af6a74b71f2e935
Mostafa VN <mostafa.vahedi.nejad@gmail.com>
2023-03-06 13:44:18 +0330

    dasd

--- mtrx.py
+++ mtrx.py
@@ -1,44 +1,46 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
+from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
-import os
-import time
-import datetime
+from time import sleep
+from datetime import datetime
 from PIL import Image
 
 updater = Updater(
     token="5594782594:AAERk4NH8Cubaj7DkWqRJohCokUP5V7FfLA", use_context=True)
 
-f =  open('caption.txt')
-caption = f.read()
 
 def start(update: Update, context: CallbackContext):
     if update.message.chat_id == 5074618670:
         while True:
-            if datetime.datetime.now().hour == 21 and datetime.datetime.now().minute == 00:
+            if datetime.now().hour == int('00') and datetime.datetime.now().minute == int('00'):
+                
+                f = open('caption.txt', "r", encoding="utf-8")
+                caption = f.read()
 
                 im1 = Image.open('carbon.png')
                 width, height = im1.size
                 im2 = Image.open('wm.png')
                 width1, height1 = im2.size
                 back_im = im1.copy()
-                back_im.paste(im2,  (width - width1 -50, height - height1 - 100), im2)
+                back_im.paste(im2,  (width - width1 - 150, height - height1 - 150), im2)
                 back_im.save('post.png', quality=95)
 
                 keyboard = InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text='Join The MTRX | ماتریکس', url='t.me/mtrx_ir')]])
-                context.bot.sendPhoto(chat_id='@mtrx_ir', photo=open('post.png', 'rb'),
-                                      caption=f'{caption}', reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
-            time.sleep(60)
+                    [[InlineKeyboardButton(text='Join The MTRX', url='t.me/mtrx_ir')]])
+                
+                context.bot.sendPhoto(
+                    chat_id='@mtrx_ir',
+                    photo=open('post.png', 'rb'),
+                    caption=f'{caption}',
+                    reply_markup=keyboard,
+                    parse_mode=ParseMode.MARKDOWN)
 
+                f.close()
+            sleep(60)
     else:
         context.bot.sendMessage(chat_id=update.message.chat_id,
                                 text='با عرض پوزش شما مجاز به ارسال پست نمیباشید')
 
+
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.start_polling()
 updater.idle()
-
-# token='5594782594:AAERk4NH8Cubaj7DkWqRJohCokUP5V7FfLA'
-# channel = '@mtrx_ir'
-# text = 'dd'
-# requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={channel}&text={text}')
